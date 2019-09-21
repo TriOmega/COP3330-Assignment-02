94cb65b0b4231c373b6edab97b60c5485d1b663b
ericd <ericd@DESKTOP-5AKC2S2.home>
2019-09-21 10:28:45 -0400

    Second Commit/Test

--- osorio_p1/src/osorio_p1/encrypt_p1.java
+++ osorio_p1/src/osorio_p1/encrypt_p1.java
@@ -1,10 +1,11 @@
 package osorio_p1;
 
-public class Osorio_p1 {
-
+public class encrypt_p1 {
+	impo
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, please work");
-		System.out.println("Please freaking let bin show");
+		System.out.println("Please freaking let bin");
 
 	}
 
