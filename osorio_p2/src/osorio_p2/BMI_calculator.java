896f8e674a7b902a957c7924baa4aee8dfecca35
ericd <ericd@DESKTOP-5AKC2S2.home>
2019-09-21 12:04:28 -0400

    Added Metric Option

--- osorio_p2/src/osorio_p2/BMI_calculator.java
+++ osorio_p2/src/osorio_p2/BMI_calculator.java
@@ -7,9 +7,9 @@ public class BMI_calculator {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		int weightInPounds;
-		int weightInKilograms;
+		double weightInKilograms;
 		int heightInInches;
-		int heightInMeters;
+		double heightInMeters;
 		double resultBMI;
 		int measureSystem;
 		
@@ -50,6 +50,32 @@ public class BMI_calculator {
 			System.out.println("Obesity = BMI of 30 or greater");
 		} else if (measureSystem == 2) {
 			System.out.printf("\nMetric BMI Calculator:\n");
+			System.out.println("Input Weight Below (kilograms - kg):");
+			weightInKilograms = scnr.nextDouble();
+			System.out.println("Input Height Below (meters - m):");
+			heightInMeters = scnr.nextDouble();
+			
+			resultBMI = ((weightInKilograms) / (heightInMeters * heightInMeters));
+			
+			System.out.printf("\nYour BMI Value is:\n");
+			System.out.printf("%.1f\n", resultBMI);
+			System.out.println("Your BMI category is:");
+			if (resultBMI < 18.5) {
+				System.out.println("Underweight");
+			} else if ((resultBMI >= 18.5) && (resultBMI <= 24.9)) {
+				System.out.println("Normal Weight");
+			} else if ((resultBMI >= 25) && (resultBMI <= 29.9)) {
+				System.out.println("Overweight");
+			} else if (resultBMI >= 30) {
+				System.out.println("Obesity");
+			}
+			
+			System.out.println("");
+			System.out.println("BMI Categories:");
+			System.out.println("Underweight = <18.5");
+			System.out.println("Normal Weight = 18.5-24.9");
+			System.out.println("Overweight = 25-29.9");
+			System.out.println("Obesity = BMI of 30 or greater");
 		} else {
 			System.out.println("\nInvalid Input.\n");
 		}
