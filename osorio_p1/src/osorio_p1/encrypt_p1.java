5ca8c520dd53b2d0894dc0a83d1aab68f78486b1
ericd <ericd@DESKTOP-5AKC2S2.home>
2019-09-21 15:21:38 -0400

    Encryption Application

--- osorio_p1/src/osorio_p1/encrypt_p1.java
+++ osorio_p1/src/osorio_p1/encrypt_p1.java
@@ -6,7 +6,34 @@ public class encrypt_p1 {
 	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		int[] dataArr = new int[4];
+		int rawData;
+		int i;
+		int tempValue;
 		
+		System.out.println("Please enter data in the form of a 4-digit number:");
+		rawData = scnr.nextInt();
+		
+		for(i = 0; i < dataArr.length; i++) {
+			dataArr[(dataArr.length) - 1 - i] = ((int) ((rawData / Math.pow(10, i)) % 10));
+		}
+		for(i = 0; i < dataArr.length; i++) {
+			dataArr[i] = ((dataArr[i] + 7) % 10);
+		}
+		
+		tempValue = dataArr[0];
+		dataArr[0] = dataArr[2];
+		dataArr[2] = tempValue;
+		
+		tempValue = dataArr[1];
+		dataArr[1] = dataArr[3];
+		dataArr[3] = tempValue;
+		
+		System.out.println("");
+		System.out.println("Encrypted Data:");
+		for(i = 0; i < dataArr.length; i++) {
+			System.out.print(dataArr[i]);
+		}
 	}
 
 }
