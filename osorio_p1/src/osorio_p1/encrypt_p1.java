5a166cbab1dc3197fe6f48ec4e51afb20c66ce64
ericd <ericd@DESKTOP-5AKC2S2.home>
2019-09-21 10:57:34 -0400

    Fourth Commit / Test

--- osorio_p1/src/osorio_p1/encrypt_p1.java
+++ osorio_p1/src/osorio_p1/encrypt_p1.java
@@ -1,11 +1,11 @@
 package osorio_p1;
 
+import java.util.Scanner;
+
 public class encrypt_p1 {
-	impo
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, please work");
-		System.out.println("Please freaking let bin");
+		Scanner scnr = new Scanner(System.in);
 
 	}
 
